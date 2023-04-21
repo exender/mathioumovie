762f365547f9805a6332d3d7d48ba78da600d3c8
b.bonnin <b.bonnin@efysoft.fr>
2023-04-21 17:24:53 +0200

    Movie details quand je clique sur une card

--- src/components/MovieCard.vue
+++ src/components/MovieCard.vue
@@ -1,97 +1,95 @@
 <template>
- <div class="bg-gray-800 rounded-md overflow-hidden shadow-md cursor-pointer" @click="viewMovieDetails(movie.id)">
-      <!-- Afficher l'affiche du film -->
-      <img :src="getImageUrl(movie.poster_path)" alt="Affiche du film" class="w-full h-40 object-cover" />
-      <div class="px-4 py-3">
-        <!-- Afficher le titre du film -->
-        <h2 class="text-lg font-semibold text-white">{{ movie.title }}</h2>
-        <!-- Afficher la date de sortie du film -->
-        <p class="text-gray-300">{{ formatDate(movie.release_date) }}</p>
-      </div>
+  <div class="bg-gray-800 rounded-md overflow-hidden shadow-md cursor-pointer" @click="viewMovieDetails(movie.id)">
+    <!-- Afficher l'affiche du film -->
+    <img :src="getImageUrl(movie.poster_path)" alt="Affiche du film" class="w-full h-40 object-cover" />
+    <div class="px-4 py-3">
+      <!-- Afficher le titre du film -->
+      <h2 class="text-lg font-semibold text-white">{{ movie.title }}</h2>
+      <!-- Afficher la date de sortie du film -->
+      <p class="text-gray-300">{{ formatDate(movie.release_date) }}</p>
     </div>
-  </template>
-  
-  <script>
+  </div>
+</template>
+
+<script>
 import router from '../router';
 
-  export default {
-    name: 'MovieCard',
-    props: {
-      movie: {
-        type: Object,
-        required: true
-      }
+export default {
+  name: 'MovieCard',
+  props: {
+    movie: {
+      type: Object,
+      required: true
+    }
+  },
+  methods: {
+    // Formater l'URL de l'image de l'affiche du film
+    getImageUrl(posterPath) {
+      return `https://image.tmdb.org/t/p/w500${posterPath}`;
     },
-    methods: {
-      // Formater l'URL de l'image de l'affiche du film
-      getImageUrl(posterPath) {
-        return `https://image.tmdb.org/t/p/w500${posterPath}`;
-      },
-      // Formater la date de sortie du film en format court (mois/année)
-      formatDate(date) {
-        if (date) {
-          const [year, month] = date.split('-');
-          return `${month}/${year}`;
-        }
-        return '';
-      },
-      // Naviguer vers la page MovieDetails.vue en utilisant Vue Router
-      viewMovieDetails(id) {
+    // Formater la date de sortie du film en format court (mois/année)
+    formatDate(date) {
+      if (date) {
+        const [year, month] = date.split('-');
+        return `${month}/${year}`;
+      }
+      return '';
+    },
+    // Naviguer vers la page MovieDetails.vue en utilisant Vue Router
+    viewMovieDetails(id) {
       router.push({ name: 'MovieDetails', params: { id }});
     },
   }
 };
+</script>
 
-  </script>
-  
-  <style scoped>
-  /* Style personnalisé pour la carte de film */
-  .bg-white {
-    background-color: #fff;
-  }
-  
-  .shadow-md {
-    box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  }
-  
-  .rounded-md {
-    border-radius: 0.375rem;
-  }
-  
-  .overflow-hidden {
-    overflow: hidden;
-  }
-  
-  .w-full {
-    width: 100%;
-  }
-  
-  .h-64 {
-    height: 16rem;
-  }
-  
-  .object-cover {
-    object-fit: cover;
-  }
-  
-  .p-4 {
-    padding: 1rem;
-  }
-  
-  .text-xl {
-    font-size: 1.25rem;
-  }
-  
-  .font-semibold {
-    font-weight: 600;
-  }
-  
-  .mb-2 {
-    margin-bottom: 0.5rem;
-  }
-  
-  .text-gray-500 {
-    color: #6b7280;
-  }
-  </style>
-  
+<style scoped>
+/* Style personnalisé pour la carte de film */
+.bg-white {
+  background-color: #fff;
+}
+
+.shadow-md {
+  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
+}
+
+.rounded-md {
+  border-radius: 0.375rem;
+}
+
+.overflow-hidden {
+  overflow: hidden;
+}
+
+.w-full {
+  width: 100%;
+}
+
+.h-64 {
+  height: 16rem;
+}
+
+.object-cover {
+  object-fit: cover;
+}
+
+.p-4 {
+  padding: 1rem;
+}
+
+.text-xl {
+  font-size: 1.25rem;
+}
+
+.font-semibold {
+  font-weight: 600;
+}
+
+.mb-2 {
+  margin-bottom: 0.5rem;
+}
+
+.text-gray-500 {
+  color: #6b7280;
+}
+</style>
--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,34 +1,102 @@
 <template>
-  <div>
-    <h1>{{ movie.title }}</h1>
-    <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title">
-    <p>{{ movie.overview }}</p>
-    <p>Année de sortie : {{ movie.release_date }}</p>
-    <p>Note moyenne : {{ movie.vote_average }}</p>
+  <div v-if="movie" class="flex flex-wrap">
+    <div class="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center">
+  <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title" class="w-1/4 h-auto shadow-lg">
+</div>
+
+    <div class="w-full md:w-1/2 lg:w-2/3 p-6">
+      <h1 class="text-2xl font-bold">{{ movie.title }}</h1>
+      <p class="text-gray-500 text-sm mt-2">{{ movie.release_date }}</p>
+      <p class="text-gray-600 mt-4">{{ movie.overview }}</p>
+      <p class="text-gray-600 mt-4">Note moyenne : {{ movie.vote_average }}</p>
+    </div>
+  </div>
+  <div v-else>
+    <p class="text-center mt-4">Chargement en cours...</p>
   </div>
 </template>
+<script>
+import axios from 'axios';
 
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    name: 'MovieDetails',
-    data() {
-      return {
-        movie: null
-      };
-    },
-    mounted() {
-      // Récupérer les détails du film en utilisant l'API de TMDB et l'id passé en paramètre
-      axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=75e981bcdd819c45eea5057ee60c7c36&language=fr-FR`)
-        .then(response => {
-          this.movie = response.data;
-        })
-        .catch(error => {
-          console.error(error);
-        });
-    }
-  };
-  </script>
-  
+export default {
+  name: 'MovieDetails',
+  data() {
+    return {
+      movie: null
+    };
+  },
+  mounted() {
+    // Récupérer les détails du film en utilisant l'API de TMDB et l'id passé en paramètre
+    axios.get(`https://api.themoviedb.org/3/movie/${this.$route.params.id}?api_key=75e981bcdd819c45eea5057ee60c7c36&language=fr-FR`)
+      .then(response => {
+        this.movie = response.data;
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+};
+</script>
+<style scoped>
+/* Style Tailwind CSS pour MovieDetails */
+
+/* Classe pour le conteneur d'image */
+.w-full {
+  width: 100%;
+}
+
+/* Classe pour la hauteur de l'image */
+.h-auto {
+  height: auto;
+}
+
+/* Classe pour l'ombre de boîte */
+.shadow-lg {
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
+}
+
+/* Classe pour le conteneur principal */
+.flex {
+  display: flex;
+}
+
+.flex-wrap {
+  flex-wrap: wrap;
+}
+
+/* Classe pour le titre */
+.text-2xl {
+  font-size: 1.5rem;
+}
+
+.font-bold {
+  font-weight: bold;
+}
+
+/* Classe pour la date de sortie */
+.text-gray-500 {
+  color: #6B7280;
+}
+
+.text-sm {
+  font-size: 0.875rem;
+}
+
+.mt-2 {
+  margin-top: 0.5rem;
+}
+
+/* Classe pour le résumé */
+.text-gray-600 {
+  color: #4B5563;
+}
+
+.mt-4 {
+  margin-top: 1rem;
+}
+
+/* Classe pour la note moyenne */
+.text-gray-600 {
+  color: #4B5563;
+}
+</style>
