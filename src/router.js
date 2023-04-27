db59f525083d8cc834b468f350a7896e9da237d2
b.bonnin <b.bonnin@efysoft.fr>
2023-04-27 11:41:36 +0200

    Ajout de l'actor view et de la page 404

--- src/router.js
+++ src/router.js
@@ -7,6 +7,11 @@ import MovieDetails from '@/views/MovieDetails.vue';
 // Importez le composant correspondant pour la page d'accueil
 import Home from '@/views/Home.vue';
 
+import NotFound from '@/views/NotFound.vue';
+
+import ActorView from '@/views/ActorView.vue';
+
+
 // Créez une instance de Vue Router avec les routes définies
 const router = createRouter({
   history: createWebHistory(),
@@ -23,7 +28,18 @@ const router = createRouter({
       name: 'Home',
       component: Home // Utilisez le composant correspondant pour la page d'accueil
     },
-    // Route pour les pages non trouvées
+    // Route pour la page 404 
+    {
+      path: '/:catchAll(.*)',
+      name: 'NotFound',
+      component: NotFound
+    },
+    // ActorView
+    {
+      path: '/actor/:id',
+      name: 'ActorView',
+      component: ActorView 
+    },
   ]
 });
 
--- src/views/MovieDetails.vue
+++ src/views/MovieDetails.vue
@@ -1,25 +1,28 @@
 <template>
   <div v-if="movie" class="flex flex-wrap">
     <div class="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center">
-      <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title" class="w-1/4 h-auto shadow-lg">
+      <img :src="'https://image.tmdb.org/t/p/w500/' + movie.poster_path" :alt="movie.title" class="w-1/4 h-auto shadow-lg rounded-md">
     </div>
 
     <div class="w-full md:w-1/2 lg:w-2/3 p-6">
-      <h1 class="text-2xl font-bold">{{ movie.title }}</h1>
-      <p class="text-gray-500 text-sm mt-2">{{ movie.release_date }}</p>
-      <p class="text-gray-600 mt-4">{{ movie.overview }}</p>
-      <p class="text-gray-600 mt-4">Note moyenne : {{ movie.vote_average }}</p>
+      <h1 class="text-3xl font-bold text-gray-900 mb-2">{{ movie.title }}</h1>
+      <p class="text-gray-500 text-lg">{{ movie.release_date }}</p>
+      <p class="text-gray-600 text-lg mt-4">{{ movie.overview }}</p>
+      <p class="text-gray-600 text-lg mt-4">Note moyenne : {{ movie.vote_average }}</p>
       
-      <div class="mt-4">
-        <h2 class="text-xl font-bold mb-2">Acteurs :</h2>
-        <div class="flex flex-wrap">
-          <div v-for="actor in movie.credits.cast" :key="actor.id" class="w-1/4 flex flex-col items-center mb-4">
-  <img v-if="actor.profile_path" :src="'https://image.tmdb.org/t/p/w200/' + actor.profile_path" :alt="actor.name" class="w-16 h-16 rounded-full mb-2">
-  <svg v-else class="w-16 h-16 rounded-full mb-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="gray" d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 21c-5.238 0-9-3.762-9-9s3.762-9 9-9 9 3.762 9 9-3.762 9-9 9z"/></svg>
-  <p class="text-gray-800 text-center">{{ actor.name }}</p>
-  <p class="text-gray-600 text-center">{{ actor.character }}</p>
-</div>
-
+      <div class="mt-8">
+        <h2 class="text-2xl font-bold text-gray-900 mb-4">Acteurs :</h2>
+        <div class="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+          <div v-for="actor in movie.credits.cast" :key="actor.id" class="flex flex-col items-center">
+            <router-link :to="{ name: 'ActorView', params: { id: actor.id } }">
+              <div class="w-32 h-32 bg-gray-200 rounded-full flex items-center justify-center overflow-hidden">
+                <img v-if="actor.profile_path" :src="'https://image.tmdb.org/t/p/w200/' + actor.profile_path" :alt="actor.name" class="w-full h-full object-cover">
+                <svg v-else class="w-16 h-16 text-gray-500" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path fill="currentColor" d="M12 0C5.373 0 0 5.373 0 12s5.373 12 12 12 12-5.373 12-12S18.627 0 12 0zm0 21c-5.238 0-9-3.762-9-9s3.762-9 9-9 9 3.762 9 9-3.762 9-9 9z"/></svg>
+              </div>
+            </router-link>
+            <p class="text-gray-900 font-medium mt-2">{{ actor.name }}</p>
+            <p class="text-gray-600 text-sm">{{ actor.character }}</p>
+          </div>
         </div>
       </div>
     </div>
@@ -28,6 +31,7 @@
     <p class="text-center mt-4">Chargement en cours...</p>
   </div>
 </template>
+
 
 <script>
 import axios from 'axios';
