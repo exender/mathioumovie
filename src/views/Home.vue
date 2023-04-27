9e0e5e23662ecc7e26a50f41e8314ec8c4d4d7c8
b.bonnin <b.bonnin@efysoft.fr>
2023-04-27 12:13:55 +0200

    feature/filtres debut des filtres

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,12 +2,15 @@
   <div class="bg-gray-900">
     <!-- En-tête -->
 
+    <!-- Filtres -->
+    <Filtres @sort-by-category="sortByCategory" @sort-by-alphabetical-order="sortByAlphabeticalOrder" />
+
     <!-- Contenu de la page -->
     <div class="container mx-auto py-8">
       <h1 class="text-4xl font-bold text-white mb-8">Les films populaires</h1>
       <div class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         <!-- Afficher la liste des films -->
-        <MovieCard v-for="movie in movies" :key="movie.id" :movie="movie" class="mb-4" @click="viewMovieDetails(movie.id)" />
+        <MovieCard v-for="movie in sortedMovies" :key="movie.id" :movie="movie" class="mb-4" @click="viewMovieDetails(movie.id)" />
       </div>
     </div>
   </div>
@@ -16,15 +19,18 @@
 <script>
 import axios from 'axios';
 import MovieCard from '../components/MovieCard.vue';
+import Filtres from '../components/Filtres.vue';
 
 export default {
   name: 'Home',
   components: {
-    MovieCard
+    MovieCard,
+    Filtres
   },
   data() {
     return {
-      movies: []
+      movies: [],
+      sortBy: null
     };
   },
   mounted() {
@@ -34,29 +40,33 @@ export default {
         this.movies = response.data.results;
       })
       .catch(error => {
-        console.error(error);
+        console.log(error);
       });
   },
   methods: {
     viewMovieDetails(movieId) {
-      // Naviguer vers la page MovieDetails.vue en utilisant Vue Router
-      this.$router.push({ name: 'MovieDetails', params: { id: movieId } });
+      this.$router.push(`/movie/${movieId}`);
+    },
+    sortByCategory(category) {
+      this.sortBy = category;
+    },
+    sortByAlphabeticalOrder() {
+      this.sortBy = 'alphabetical-order';
+    }
+  },
+  computed: {
+  sortedMovies: function() {
+    if (this.sortBy === 'alphabetical-order') {
+      // trier les films par ordre alphabétique
+    } else if (this.sortBy) {
+      // trier les films en fonction de la catégorie sélectionnée
+      return this.movies.filter(movie => movie.genre_ids.includes(parseInt(this.sortBy)));
+    } else {
+      // afficher tous les films
+      return this.movies;
     }
   }
+}
 };
 </script>
 
-<style scoped>
-/* Style personnalisé pour le contenu de la page */
-.container {
-  /* Utiliser toute la largeur disponible */
-  width: 100%;
-  /* Définir une marge intérieure */
-  padding-left: 1rem;
-  padding-right: 1rem;
-}
-
-.text-white {
-  color: #fff;
-}
-</style>
